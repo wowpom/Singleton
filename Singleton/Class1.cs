44419cda0fecb8bc8c4b4ec159801027d4f727bd
Valik0805 <56127421+Valik0805@users.noreply.github.com>
2020-05-01 13:47:07 +0700

    Добавьте файлы проекта.

---
+++ Singleton/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace Singleton
+{
+    public class SingletonExample
+    {
+        private static SingletonExample singletonExample;
+
+        private SingletonExample()
+        {
+        }
+
+        public static SingletonExample GetInstance()
+        {
+            // для исключения возможности создания двух объектов 
+            if (singletonExample == null)
+            {
+                lock (typeof(ConfigurationManager))
+                {
+                    if (singletonExample == null)
+                        singletonExample = new SingletonExample();
+                }
+            }
+
+            return singletonExample;
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xceed.Wpf.Toolkit;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingletonExample singletonExample = SingletonExample.GetInstance();
+            SingletonExample singletonExample2 = SingletonExample.GetInstance();
+            if (Object.ReferenceEquals(singletonExample, singletonExample2))
+                Console.WriteLine("Обе ссылки ссылаются на один объект");
+        }
+    }
+}
